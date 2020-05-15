84ad4c7fafdeb629b4dbad8a730e63f04dd3b681
Vadim <62276259+vadimvrabie@users.noreply.github.com>
2020-05-15 12:32:20 +0300

    tasc 1 schimbari

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,10 @@
-import modals from "./modules/modals.js"
+import modals from "./modules/modals"
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
     modals();
 });
-
+import {slider} from "./slaider"
+window.addEventListener('DOMContentLoaded', () => {
+    "use strict";
+    slider();
+});
