fdee9538c3285dab57b2fbd39fb9210daff874fe
Filipopolski Serghei <filipopolskiserghei1994@gmail.com>
2020-05-19 22:19:00 +0300

    task10

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,18 @@
 import modals from "./modules/modals.js"
 import slider from "./modules/slide"
+import sliderAndButton from "./modules/task10.js"
+
+
 window.addEventListener('DOMContentLoaded', () => {
     "use strict";
     modals();
     slider('.main-slider-item',"verticalInDown");
     // slider('.main-slider-item',"verticalUpDown");
-    // slider('.main-slider-item',"orizontalLeft");
+    // slider('.feedback-slider-item',"orizontalLeft");
     // slider('.main-slider-item',"orizontalRight");
 
-});
+    sliderAndButton();
 
+
+});
 
--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,3 +1,5 @@
+import slider from "./slide";
+
 const modals = () => {
     function bindModal(triggerSelector, modalSelector, closeSelector) {
         const trigger = document.querySelectorAll(triggerSelector),
@@ -37,27 +39,113 @@ const modals = () => {
     //     }, time);
     // }
 
-    bindModal('.button-design', '.popup-design','.popup-design .popup-close');
+    bindModal('.button-design', '.popup-design', '.popup-design .popup-close');
     bindModal('.button-consultation', '.popup-consultation', '.popup-consultation .popup-close');
-
 
 
     const gift = document.querySelector(".fixed-gift");
     gift.addEventListener("click", () => {
         return gift.style.display = "none";
     });
-    bindModal('.fixed-gift', '.popup-gift','.popup-gift .popup-close ');
-
-
-
-
-
-    // showModalByTime('.popup-design', 60000);
-
-
+    bindModal('.fixed-gift', '.popup-gift', '.popup-gift .popup-close ');
 
 
 };
 
-export default modals;
+    // showModalByTime('.popup-design', 60000);
+
+
+// ------------------------------------------------------------------------------------
+
+//                                        Task 10
+
+//                                     for slider
+
+// function sliderAndButton () {
+//
+//     let slides = document.querySelectorAll(".feedback-slider-item"),
+//         arrowLeft = document.querySelector(".main-prev-btn"),
+//         arrowRight = document.querySelector(".main-next-btn"),
+//         mouse = document.querySelector(".feedback-slider"),
+//         current = 0;
+//
+//     showSlides();
+//
+//     function showSlides() {
+//
+//         let i;
+//         for (i = 0; i < slides.length; i++) {
+//             slides[i].style.display = "none";
+//         }
+//         current++;
+//         if (current > slides.length) {
+//             current = 1;
+//         }
+//
+//         slides[current - 1].style.display = "block";
+//     }
+//
+//     let slideInterval = setInterval(showSlides, 2000);
+//
+//     //                                       for button
+//
+// // Clear all images
+//     function reset() {
+//         for (let i = 0; i < slides.length; i++) {
+//             slides[i].style.display = "none";
+//         }
+//     }
+//
+// // Show prev
+//     function slideLeft() {
+//         reset();
+//         slides[current - 1].style.display = "block";
+//         current--;
+//     }
+//
+// // Show next
+//     function slideRight() {
+//         reset();
+//         slides[current + 1].style.display = "block";
+//         current++;
+//     }
+//
+// // Left arrow click
+//     arrowLeft.addEventListener("click", function () {
+//         if (current === 0) {
+//             current = slides.length;
+//         }
+//         slideLeft();
+//     });
+//
+// // Right arrow click
+//     arrowRight.addEventListener("click", function () {
+//         if (current === slides.length - 1) {
+//             current = -1;
+//         }
+//         slideRight();
+//     });
+//
+// //                         for mouse
+//
+//     function pauseSlideshow() {
+//         clearTimeout(slideInterval);
+//     }
+//
+//     function playSlideshow() {
+//         slideInterval = setInterval(showSlides, 2000);
+//     }
+//
+//     mouse.addEventListener("mouseover", () => {
+//         pauseSlideshow();
+//     });
+//
+//     mouse.addEventListener("mouseout", () => {
+//         playSlideshow();
+//     });
+// }
+//
+// sliderAndButton();
+
+export default modals
 
